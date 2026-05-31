69ffa1d0b5dc67371d51906c8af7354cc0d41e12
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-05-31 14:40:20 +0530

    add update command

--- expense_tracker/app.js
+++ expense_tracker/app.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 const path = 'expenses.json';
 let data = []
-let validCommands = ['add', 'delete', 'list', 'summary']
+let validCommands = ['add', 'delete', 'list', 'summary', 'update']
 let validFlags = ['id', 'description', 'amount', 'month']
 
 if (fs.existsSync(path)) data = JSON.parse(fs.readFileSync(path, 'utf8'));
@@ -32,7 +32,7 @@ function validateId(expenseId) {
 
 function getMonthName(monthNum) {
     if (monthNum >= 1 && monthNum <= 12){
-        return new Date(2026, monthNum).toLocaleString("en-US", { month: "short", timeZone: "UTC" });
+        return new Date(2026, monthNum - 1).toLocaleString("en-US", { month: "short", timeZone: "UTC" });
     }
     else {
         console.error(`Invalid month provided (month: ${monthNum}).`);
@@ -40,9 +40,14 @@ function getMonthName(monthNum) {
     }
 }
 
+function getExpense(expenseId) {
+    let expense = data.find(expense => expense.id === expenseId);
+    return expense;
+}
+
 if (command === 'add') {
     let dateMade = new Date().toISOString();
-    if(flags.description && flags.amount) {
+    if(flags.description && flags.amount && flags.amount > 0) {
         let expense = {
             'id': data.length + 1,
             'date': dateMade,
@@ -72,9 +77,39 @@ if (command === 'delete') {
     }
 };
 
-if (command === 'list') {
-    console.table(data)
-};
+if (command === 'update') {
+    if (flags.id) {
+        validateId(parseInt(flags.id));
+        
+        if (flags.description && flags.amount){
+            let expense = getExpense(parseInt(flags.id));
+            expense.description = flags.description;
+            expense.amount = parseInt(flags.amount);
+            fs.writeFileSync(path, JSON.stringify(data));
+            console.log(`Expense description and amount updated successfully. (ID: ${parseInt(flags.id)})`);
+        }
+        else if(flags.description) {
+            let expense = getExpense(parseInt(flags.id));
+            expense.description = flags.description;
+            fs.writeFileSync(path, JSON.stringify(data));
+            console.log(`Expense description updated successfully. (ID: ${parseInt(flags.id)})`);
+        } 
+        else if (flags.amount){
+            let expense = getExpense(parseInt(flags.id));
+            expense.amount = parseInt(flags.amount);
+            fs.writeFileSync(path, JSON.stringify(data));
+            console.log(`Expense amount updated successfully. (ID: ${parseInt(flags.id)})`);
+        }
+        else {
+            console.error(`New expense --description or --amount not provided.`);
+            process.exit(1);
+        }
+    }
+    else {
+        console.error(`Expense --id not provided.`);
+        process.exit(1);
+    }
+}
 
 if (command === 'summary') {
     let total = 0;    
@@ -87,6 +122,8 @@ if (command === 'summary') {
         console.log(`Total expenses: $${total}.`)
     }
 };
+
+if (command === 'list') {console.table(data)};
 
 // fallback funct
 if (!validCommands.includes(command)) {
