744abf9501ca4db3d08f4ef84c4564a97676ca47
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-06-14 16:57:53 +0530

    fix: fetch weather data from API instead of using hardcoded values
    - Fetch weather data from API instead of using hardcoded values.
    - Added error handling for API requests.
    - Updated response caching mechanism with Redis.
    - Modified weather object structure based on API response.

--- apps/weather_api/server.js
+++ apps/weather_api/server.js
@@ -26,18 +26,32 @@ app.get("/api/weather", async (req, res) => {
         });
     }
 
-    const weather = {
-        "city": city,
-        "temperature": 25,
-        "humidity": 72,
-        "conditions": "Sunny",
-        "wind_speed": 12.3,
-        "cached": false
+    // Visualcrossing URL format
+    // {BASE_URL}/{city}?key={API_KEY}&unitGroup=metric&include=current
+    try {
+        const response = await fetch(`${process.env.WEATHER_API_BASE_URL}/${city}?key=${process.env.WEATHER_API_KEY}&unitGroup=metric&include=current`)
+        if (response.status === 400) {
+            return res.status(404).json({ error: "404 - city not found" });
+        } else if (!response.ok) {
+            return res.status(500).json({ error: "Weather service is unavailable" });
+        }
+        const data = await response.json();
+        const weather = {
+            city: city,
+            temperature: data.currentConditions.temp,
+            humidity: data.currentConditions.humidity,
+            conditions: data.currentConditions.conditions,
+            wind_speed: data.currentConditions.windspeed,
+            cached: false
+        };
+        await redisClient.set(cachedKey, JSON.stringify(weather), { EX: Number(process.env.CACHE_TTL_SECONDS) || 60 * 60 * 12 })
+        res.json(weather);
+    } catch (error) {
+        console.error("Error fetching weather:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
-    await redisClient.set(cachedKey, JSON.stringify(weather), { EX: Number(process.env.CACHE_TTL_SECONDS) })
-
-    res.json(weather);
 })
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
