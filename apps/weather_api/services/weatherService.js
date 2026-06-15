2bf0074dc1b66b49d5f45516e903214984b22293
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-06-15 13:01:58 +0530

    feat: add fetchWeather function for retrieving weather data

---
+++ apps/weather_api/services/weatherService.js
@@ -0,0 +1,19 @@
+export async function fetchWeather(city) {
+    try {
+        const weatherRes = await fetch(`${process.env.WEATHER_API_BASE_URL}/${city}?key=${process.env.WEATHER_API_KEY}&unitGroup=metric&include=current`);
+        if (weatherRes.status === 400) { return { error: "404 - city not found", status: 404 }; }
+        else if (!weatherRes.ok) { return { error: "Weather service is unavailable", status: 502 }; }
+
+        const weatherData = await weatherRes.json();
+        return {
+            city: city,
+            temperature: weatherData.currentConditions.temp,
+            humidity: weatherData.currentConditions.humidity,
+            conditions: weatherData.currentConditions.conditions,
+            wind_speed: weatherData.currentConditions.windspeed,
+        };
+    } catch (error) {
+        console.error("Error fetching weather:", error);
+        return { error: "Internal server error", status: 500 };
+    }
+}
