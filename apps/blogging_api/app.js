b2455272cbd47d14c39f1dcfe2a37624f40669e1
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-06-19 15:10:21 +0530

    fix: remove server start from app.js

--- apps/blogging_api/app.js
+++ apps/blogging_api/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 import errorHandler from "./middleware/errorHandler.js";
-import { initializeDatabase } from "./db/index.js";
 
 dotenv.config();
 
@@ -13,12 +12,5 @@ app.use(express.json());
 app.use("/api/posts", postRoutes);
 
 app.use(errorHandler);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, async () => {
-    await initializeDatabase();
-    console.log(`Server running on port ${port}`);
-});
 
 export default app;
