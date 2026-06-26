cd6c4ecb808a41d2e951f27d6297b54f123bfddf
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-06-26 13:38:49 +0530

    feat: add todo routes and error handler middleware

--- apps/todo_list_api/src/app.js
+++ apps/todo_list_api/src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
+
+// Middleware
 app.use(express.json());
+
+// Routes
 app.use(authRoutes);
+app.use(todoRoutes);
+
+// Error Handler
+app.use(errorHandler);
 
 export default app;
