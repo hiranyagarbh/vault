373d569fcdd103b20893cef870005cd1da797192
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-07-10 08:44:32 +0530

    feat: add cookie parser and middleware configurations

--- apps/expense_tracker_api/src/app.js
+++ apps/expense_tracker_api/src/app.js
@@ -1,9 +1,13 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
-// Error Handler
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(errorHandler);
 
 export default app;
