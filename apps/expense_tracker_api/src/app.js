665e3a995089864f47a10296d802933820edcf47
Hiranya <hiranyagarbhchoudhary@gmail.com>
2026-07-11 13:46:44 +0530

    refactor: reorder middleware and routes in app.js

--- apps/expense_tracker_api/src/app.js
+++ apps/expense_tracker_api/src/app.js
@@ -1,13 +1,19 @@
-import cookieParser from "cookie-parser";
 import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
+// Middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
+// Routes
+app.use(authRoutes);
+
+// Error Handler
 app.use(errorHandler);
 
 export default app;
